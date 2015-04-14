6c065a407e82abc1c1a9e02626e69270d29ffc17
Tom Coquereau <tom@thau.me>
2015-04-14 11:12:54 +0200

    Add background video

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  /* Video Youtube
+  ----------------------------------------------------------------------*/
+  if($('.bg-youtube-video').length) {
+
+    $('.bg-youtube-video .overlay').tubular({
+      videoId: '_JFx9Or2Voo',
+      start: 3,
+      mute: true
+    });
+  }
+});
