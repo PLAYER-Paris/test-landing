b57379dcb124981ba845491309da9cbbab8ba072
Tom Coquereau <tom@thau.me>
2015-04-15 12:30:38 +0200

    Add responsive

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
 
   /* Video Youtube
   ----------------------------------------------------------------------*/
-  if($('.bg-youtube-video').length) {
+  var shouldPlayVideo = $(window).width() > 450;
 
+  if (shouldPlayVideo) {
     $('.bg-youtube-video .overlay').tubular({
       videoId: 'C5CSVSuUVxY',
       start: 3,
       mute: true
     });
+
+  } else {
+    $('.part-0').addClass('replacement-bg-youtube');
   }
 });
